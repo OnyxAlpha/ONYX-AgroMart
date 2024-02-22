06dfefcb536b266ace120c2d1143dedfb8c1d516
D'Angelo <dangelokwakye@gmail.com>
2024-02-22 08:36:28 +0000

    adding page routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { Navbar } from "./pages/landing/header/index";
+import { Landing } from "./pages/landing/landing";
+import { About } from "./pages/about/index";
+import { Products } from "./pages/products/index";
+import { Signup } from "./pages/signup/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Router>;
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+   </>
   );
 }
 
---
+++ src/pages/landing/landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './hero/index';
+import Products from './products/index'
+import Footer from './footer/index';
+
+function Landing () {
+  return (
+    <section>
+        <Hero />
+        <Products />
+        <Footer />
+    </section>
+  )
+}
+export default Landing;
