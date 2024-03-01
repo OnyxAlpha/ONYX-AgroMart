1a10728ddedabd4d7b98763f85ac50d92f8ee604
Hannan-Laaraba <ashraweelaaraba@gmail.com>
2024-03-01 13:19:28 +0000

    Billing Form added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./pages/landing/footer/footer.js";
 import Signup from "./pages/signup/signup.js";
 import Products from "./components/produce.jsx";
 import ProductUploadForm from "./pages/form/productupload.jsx";
+import Billingform from "./pages/billing_form/billingform.js";
+import BillingFormWithPayment from "./pages/billing_form/billingform.js";
 
 
 
@@ -13,6 +15,7 @@ function App() {
        <Footer />
        <Signup />
        <ProductUploadForm />
+       <BillingFormWithPayment />
     </React.Fragment>
    
   );
